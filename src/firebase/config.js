60c43ca485987f29a2b66a1e0bf730e505e90cad
Jesus LM <76732722+Jesus-LM@users.noreply.github.com>
2025-05-04 16:01:37 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzxl2FNJFgbY_Qb7jVE6CUTm9bvaZTYoY",
-    authDomain: "desayunos-ai-app.firebaseapp.com",
-    projectId: "desayunos-ai-app",
-    storageBucket: "desayunos-ai-app.firebasestorage.app",
-    messagingSenderId: "665043402932",
-    appId: "1:665043402932:web:714666613928802f87d7e1"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Inicializar Firebase
